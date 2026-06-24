1bd3952f5c774c16239f3d17941c8a63c3ee8b96
TheCryptoArborist <ptshopping63@gmail.com>
2026-06-23 20:59:30 -0500

    Drop speculative consultation indexes

---
+++ supabase/migrations/005_drop_speculative_consultation_indexes.sql
@@ -0,0 +1,4 @@
+drop index if exists consultation_requests_created_idx;
+drop index if exists consultation_requests_status_idx;
+drop index if exists consultation_reports_code_idx;
+drop index if exists consultation_reports_issued_idx;
