159e975acd5e5c55cfe2b1d9613cc8f788c7e8de
TheCryptoArborist <ptshopping63@gmail.com>
2026-07-04 09:46:25 -0500

    Remove duplicate credentials section render

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import { CredentialsSection } from "@/components/CredentialsSection";
 import { TheArboristHomePage } from "@/components/TheArboristHomePage";
 
 export default function HomePage() {
-  return (
-    <div>
-      <TheArboristHomePage />
-      <CredentialsSection />
-    </div>
-  );
+  return <TheArboristHomePage />;
 }
