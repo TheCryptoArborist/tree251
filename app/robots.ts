e4db86dce6cca5e42fa7ef09a9797e3bb6d7c5b4
TheCryptoArborist <ptshopping63@gmail.com>
2026-07-15 11:30:33 -0500

    Add robots configuration and sitemap reference

---
+++ app/robots.ts
@@ -0,0 +1,14 @@
+import type { MetadataRoute } from "next";
+
+const siteUrl = "https://tree251.xyz";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/"
+    },
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl
+  };
+}
